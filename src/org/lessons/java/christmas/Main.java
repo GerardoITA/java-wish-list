154d04ef6fc93a6e83e0efefb26f5146e48dd5cb
GerardoITA <gerardo.giusti@tiscali.it>
2023-06-09 16:50:53 +0200

    update2

--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -1,15 +1,15 @@
 package org.lessons.java.christmas;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean stop = false;
-        int n = 0;
-        Map<Integer, String> listaRegali = new HashMap<>();
+
+        List<String> listaRegali = new ArrayList<>();
 
         while (!stop) {
             System.out.println("Che regalo vuoi aggiungere? ( 'stop' per fermare )");
@@ -17,10 +17,12 @@ public class Main {
             if (choice.equalsIgnoreCase( "stop")) {
                 stop = true;
             } else {
-                listaRegali.put(n, choice);
-                n++;
-                System.out.println(listaRegali.size());
+                listaRegali.add(choice);
             }
+        }
+
+        for (int i = 0; i < listaRegali.size(); i++) {
+            System.out.println(listaRegali.get(i));
         }
 
 
